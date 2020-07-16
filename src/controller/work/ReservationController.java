c51a560140e61fcdb566f4059c5b24b3384f7b68
tchamouramses <tchamouramses@gmail.com>
2020-07-16 22:02:47 +0200

    comit pour le pull

--- src/controller/work/ReservationController.java
+++ src/controller/work/ReservationController.java
@@ -1,7 +1,30 @@
 package controller.work;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.work.entities.Reservation;
+import model.work.interfaces.implementation.reservationImpl;
 
 public class ReservationController extends HttpServlet {
+	reservationImpl Reservation;
+	private Reservation c = null;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/reservation.jsp")
+				.forward(request, response);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
 
 }
